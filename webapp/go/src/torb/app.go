b167b293a9a40b34237e931c3a0d756dce19e5d8
SAKATA Sinji <sinji@sakasin.net>
2018-09-15 16:56:35 +0900

    hoge

--- webapp/go/src/torb/app.go
+++ webapp/go/src/torb/app.go
@@ -244,7 +244,7 @@ func getEvent(eventID, loginUserID int64) (*Event, error) {
 		"C": &Sheets{},
 	}
 
-	rows, err := db.Query("SELECT S.id, S.rank, S.num, S.price, R.id, R.user_id, MIN(R.reserved_at) FROM reservations as R, sheets as S WHERE R.sheet_id = S.id AND event_id = ? AND canceled_at IS NULL GROUP BY sheet_id ORDER BY S.id ASC", eventID)
+	rows, err := db.Query("SELECT S.id, S.rank, S.num, S.price, R.id, R.user_id, R.reserved_at FROM (SELECT * FROM sheets ORDER BY `rank`, num) as S LEFT OUTER JOIN (select * from reservations WHERE event_id = ? AND canceled_at IS NULL GROUP BY event_id, sheet_id) as R ON R.sheet_id = S.id", eventID)
 	if err != nil {
 		log.Fatal(err)
 		return nil, err
